1f959243eb3d35ea697dd3668ee1a340878ce0fd
Nazar Yablonskyi <nazar@bitwala.com>
2019-11-08 10:32:47 +0200

    demo app: upgrade to rn 0.61.4

--- demo/ReactNativeIDnowSample/App.tsx
+++ demo/ReactNativeIDnowSample/App.tsx
@@ -8,16 +8,16 @@
  * @format
  */
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-import { IDnowManager } from 'react-native-idnow';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
+import {IDnowManager} from 'react-native-idnow';
 
 interface Props {}
 export default class App extends Component<Props> {
   render() {
     const options = {
       showVideoOverviewCheck: true,
-      transactionToken: 'TST-PTSSG',
+      transactionToken: 'TST-QAFWM',
       environment: 'TEST',
     };
     return (
