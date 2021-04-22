21f7a7f65bfba339c9ed3ad88f863c2c0682454f
ayo.akingbule <ayo.akingbule@moonfare.com>
2021-04-22 15:22:04 +0200

    IdentifictionCompleteScreen Clean up

--- demo/MoonfareApp/src/screens/IdentificationCompleteScreen/IdentifictionCompleteScreen.js
+++ demo/MoonfareApp/src/screens/IdentificationCompleteScreen/IdentifictionCompleteScreen.js
@@ -60,28 +60,28 @@ const IdentificationCompleteComponent = ({navigation, eva}) => {
           height: Dimensions.get('window').height,
         }}>
         <View style={style.logoView}>
-          <Image style={style.logo} source={images.logo} />
+          <Image style={style.logo} source={images.appLogo} />
         </View>
 
         <View style={style.titleView}>
-          <CustomText style={style.titleText}>
+          <CustomText style={style.titleText} weight={500}>
             {i18n.t('IDENTIFICATION_COMPLETE.IDENTIFICATION')}
           </CustomText>
         </View>
 
         <View style={style.titleView}>
-          <CustomText style={style.titleText}>
-            {i18n.t('IDENTIFICATION_COMPLETE.EnterIdentID')}
+          <CustomText style={style.successTitleText}>
+            {i18n.t('IDENTIFICATION_COMPLETE.SUCCESS_MESSAGE')}
           </CustomText>
         </View>
 
-        <View style={style.loginButtonView}>
+        <View style={style.learnMoreButtonView}>
           <LoaderButton
-            style={style.loginButton}
-            textStyle={style.buttonTextStyle}
+            style={style.learnMoreButton}
+            textStyle={style.textStyle}
             onPress={() => onPress()}
             size="large"
-            text={i18n.t('LOGIN.LOGIN')}
+            text={i18n.t('IDENTIFICATION_COMPLETE.LEARN_MORE')}
           />
         </View>
       </ScrollView>
